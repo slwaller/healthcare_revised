d6bf364b9d82aa75a204d31eea2819bc05396b09
Sam Waller <samuellwaller@gmail.com>
2015-10-30 01:57:36 -0400

    getting rid of dr for a second

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -39,8 +39,25 @@ class HospitalsController < ApplicationController
     redirect_to hospitals_path
   end
 
+  def create_doctor
+    @hospital = Hospital.find params[:id]
+    @hospital.doctors.create doctor_params
+    redirect_to hospital_path(@hospital)
+  end
+
+  def delete_doctor
+    @hospital = Hospital.find params[:id]
+    @doctor = Doctor.find params[:doctor_id]
+    @doctor.delete
+    redirect_to @hospital
+  end
+
   private
   def hospital_params
     params.require(:hospital).permit(:name, :address, :description)
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:dr_name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305192240) do
+ActiveRecord::Schema.define(version: 20151006204933) do
+
+  create_table "doctors", force: :cascade do |t|
+    t.string   "dr_name",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "hospitals", force: :cascade do |t|
     t.string   "name",        limit: 255
